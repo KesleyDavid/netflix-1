7ba1d628e79e71b223198b027dfdefbce73114d3
Willian Tavares <willianliketavares@gmail.com>
2020-05-01 15:53:52 -0300

    first version of the homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
+import { FiPlus } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 
-import { Container, Header, Button, Main, HeaderContent, SignUp, Input, SignUpButton, EnjoyTv } from './styles';
+import {
+  Container,
+  Header,
+  Button,
+  Main,
+  HeaderContent,
+  SignUp,
+  Input,
+  SignUpButton,
+  EnjoyOnTv,
+  EnjoyOnTvText,
+  EnjoyOnTvGif,
+  Download,
+  DownloadGif,
+  DownloadText,
+  DownloadController,
+  Watch,
+  WatchText,
+  WatchGif,
+  FAQ,
+  Footer,
+} from './styles';
 
-const Home: React.FC = props => {
+const Home: React.FC = () => {
   return (
     <Container>
       <Main>
         <Header>
-          <img src={logo} alt="Netflix" style={{ width: '180px' }}/>
+          <img src={logo} alt="Netflix" style={{ width: '180px' }} />
           <Button>Entrar</Button>
         </Header>
 
@@ -19,20 +41,167 @@ const Home: React.FC = props => {
           <h3>Assista onde quiser. Cancele quando quiser.</h3>
 
           <SignUp>
-            <Input placeholder="Email"/>
+            <Input placeholder="Email" />
             <SignUpButton>ASSINE A NETFLIX</SignUpButton>
           </SignUp>
 
-          <p>Pronto para assistir? Informe seu email para criar ou acessar sua conta.</p>
+          <p>
+            Pronto para assistir? Informe seu email para criar ou acessar sua
+            conta.
+          </p>
         </HeaderContent>
       </Main>
+      <EnjoyOnTv>
+        <EnjoyOnTvText>
+          <h1>Aproveite na TV.</h1>
+          <h3>
+            Assista em Smart TVs, Playstation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros aparelhos
+          </h3>
+        </EnjoyOnTvText>
+        <EnjoyOnTvGif>
+          <img
+            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+            alt="tv"
+          />
 
+          <video autoPlay playsInline muted loop>
+            <source
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv.m4v"
+              type="video/mp4"
+            />
+          </video>
+        </EnjoyOnTvGif>
+      </EnjoyOnTv>
+      <Download>
+        <DownloadGif>
+          <img
+            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg"
+            alt="Eleven"
+          />
 
-      <EnjoyTv>
+          <DownloadController>
+            <img
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png"
+              alt="Stranger Things"
+            />
+            <div>
+              <h3>Stranger Things</h3>
+              <p>Download em andamento...</p>
+            </div>
 
-      </EnjoyTv>
+            <img
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/download-icon.gif"
+              alt="Download Gif"
+              style={{ width: '60px', height: '60px' }}
+            />
+          </DownloadController>
+        </DownloadGif>
+        <DownloadText>
+          <h1>Baixe séries para assistir offline.</h1>
+          <h3>
+            Salve seus títulos favoritos e sempre tenha algo para assistir.
+          </h3>
+        </DownloadText>
+      </Download>
+      <Watch>
+        <WatchText>
+          <h1>Assista quando quiser</h1>
+          <h3>
+            Assista no celular, tablet, smart TV ou notebook sem pagar a mais
+            por isso
+          </h3>
+        </WatchText>
+        <WatchGif>
+          <img
+            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png"
+            alt="Netflix on ipad, mac and phone"
+          />
+
+          <video autoPlay playsInline muted loop>
+            <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v" />
+          </video>
+        </WatchGif>
+      </Watch>
+      <FAQ>
+        <h1>Perguntas frequentes</h1>
+        <button>
+          <h1>O que é a Netflix?</h1>
+          <FiPlus size={40} color="#fff" style={{ marginLeft: 'auto' }} />
+        </button>
+
+        <button>
+          <h1>Quanto custa a Netflix?</h1>
+          <FiPlus size={40} color="#fff" style={{ marginLeft: 'auto' }} />
+        </button>
+
+        <button>
+          <h1>Onde posso assistir?</h1>
+          <FiPlus size={40} color="#fff" style={{ marginLeft: 'auto' }} />
+        </button>
+
+        <button>
+          <h1>Como faço para cancelar?</h1>
+          <FiPlus size={40} color="#fff" style={{ marginLeft: 'auto' }} />
+        </button>
+
+        <button>
+          <h1>O que eu posso assistir na Netflix?</h1>
+          <FiPlus size={40} color="#fff" style={{ marginLeft: 'auto' }} />
+        </button>
+
+        <div>
+          <SignUp>
+            <Input placeholder="Email" />
+            <SignUpButton>ASSINE A NETFLIX</SignUpButton>
+          </SignUp>
+        </div>
+
+        <p>
+          Pronto para assistir? Informe seu email para criar ou acessar sua
+          conta.
+        </p>
+      </FAQ>
+      <Footer>
+        <h3>Dúvidas? Entre em contato.</h3>
+        <div>
+          <div>
+            <ul>
+              <li>Perguntas frequentes</li>
+              <li>Relações com investidores</li>
+              <li>Formas de assistir</li>
+              <li>Informações corporativas</li>
+              <li>Originais Netflix</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Centro de ajuda</li>
+              <li>Carreiras</li>
+              <li>Termos de uso</li>
+              <li>Entre em contato</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Conta</li>
+              <li>Resgatar cartão pré-pago</li>
+              <li>Privacidade</li>
+              <li>Teste de velocidade</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Imprensa</li>
+              <li>Comprar cartão pré-pago</li>
+              <li>Preferência de cookies</li>
+              <li>Avisos legais</li>
+            </ul>
+          </div>
+        </div>
+      </Footer>
     </Container>
   );
-}
+};
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import backgroundImage from '../../assets/painel.jpeg'
+import backgroundImage from '../../assets/painel.jpeg';
 
 export const Container = styled.div`
   height: 100vh;
@@ -11,7 +11,7 @@ export const Main = styled.main`
   background: url(${backgroundImage});
   background-size: cover;
   height: 100%;
-  
+
   &::after {
     content: '';
     position: absolute;
@@ -20,7 +20,12 @@ export const Main = styled.main`
     height: 1348px;
     width: 100%;
     z-index: 1;
-    background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%);
+    background-image: linear-gradient(
+      to top,
+      rgba(0, 0, 0, 0.8) 0%,
+      rgba(0, 0, 0, 0) 60%,
+      rgba(0, 0, 0, 0.8) 100%
+    );
     background-color: rgba(0, 0, 0, 0.4);
     height: 100vh;
   }
@@ -106,6 +111,260 @@ export const SignUpButton = styled.button`
   border: 0;
 `;
 
-export const EnjoyTv = styled.div`
-  height: 1000px;
+export const EnjoyOnTv = styled.div`
+  border-top: 10px solid #222;
+
+  height: 600px;
+  display: flex;
+  justify-content: space-around;
+  padding: 80px;
+  width: 100%;
+`;
+
+export const EnjoyOnTvText = styled.div`
+  color: white;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  h1 {
+    font-size: 4em;
+    max-width: 1000px;
+  }
+
+  h3 {
+    font-size: 2em;
+    font-weight: 400;
+    margin-top: 20px;
+  }
+`;
+
+export const EnjoyOnTvGif = styled.div`
+  display: flex;
+  display: relative;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 640px;
+    z-index: 2;
+    height: 480px;
+  }
+
+  video {
+    position: absolute;
+    top: 118%;
+  }
+`;
+
+export const Download = styled.div`
+  border-top: 10px solid #222;
+
+  height: 700px;
+  display: flex;
+  justify-content: space-around;
+  padding: 60px;
+  padding-top: 40px;
+  width: 100%;
+`;
+
+export const DownloadGif = styled.div`
+  display: flex;
+  display: relative;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+export const DownloadText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  h1 {
+    font-size: 4em;
+    color: white;
+    max-width: 600px;
+    padding-bottom: 6px;
+  }
+
+  h3 {
+    font-size: 2em;
+    width: 650px;
+    font-weight: regular;
+    color: white;
+  }
+`;
+
+export const DownloadController = styled.div`
+  width: 450px;
+  height: 120px;
+  border: 3px solid rgba(255, 255, 255, 0.25);
+  border-radius: 20px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding-left: 10px;
+
+  position: absolute;
+  z-index: 2;
+  background-color: black;
+  bottom: 15%;
+
+  img {
+    height: 80%;
+    margin-right: 20px;
+  }
+
+  h3 {
+    font-size: 1.4em;
+    width: calc(100% - 30px);
+    color: white;
+    padding-bottom: 6px;
+  }
+
+  p {
+    font-size: 1.2em;
+    font-weight: regular;
+    color: blue;
+  }
+`;
+
+export const Watch = styled.div`
+  border-top: 10px solid #222;
+
+  height: 700px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 80px;
+  width: 100%;
+`;
+
+export const WatchText = styled.div`
+  h1 {
+    font-size: 4em;
+    color: white;
+    max-width: 600px;
+    padding-bottom: 6px;
+  }
+
+  h3 {
+    font-size: 2em;
+    width: 650px;
+    font-weight: regular;
+    color: white;
+  }
+`;
+
+export const WatchGif = styled.div`
+  display: flex;
+
+  img {
+    height: 480px;
+    width: 640px;
+    z-index: 2;
+  }
+
+  video {
+    position: absolute;
+    width: 400px;
+    right: 15%;
+    top: 277%;
+    z-index: 1;
+  }
+`;
+
+export const FAQ = styled.div`
+  border-top: 10px solid #222;
+
+  height: 1200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 250px;
+  width: 100%;
+
+  p {
+    color: white;
+    font-size: 1.6em;
+    margin: 20px 0 0 0;
+  }
+
+  > h1 {
+    color: white;
+    font-size: 4em;
+    margin-bottom: 60px;
+    font-weight: 500;
+  }
+
+  > button {
+    display: flex;
+
+    background-color: #303030;
+    border: 0;
+    height: 110px;
+    width: 100%;
+    padding: 0 40px;
+
+    h1 {
+      color: white;
+      font-size: 2.8em;
+      font-weight: 400;
+      text-align: left;
+    }
+
+    & + button {
+      margin-top: 10px;
+    }
+  }
+
+  div {
+    margin-top: 40px;
+    width: 96%;
+
+    input {
+      height: 90px;
+    }
+  }
+`;
+
+export const Footer = styled.footer`
+  border-top: 10px solid #222;
+
+  height: 600px;
+  width: 100%;
+
+  color: #757575;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  h3 {
+    font-size: 1.5em;
+    font-weight: 400;
+  }
+
+  div {
+    display: flex;
+    margin: 30px;
+    font-weight: 400px;
+
+    div {
+      margin: 20px 70px;
+
+      li {
+        list-style: none;
+
+        & + li {
+          margin-top: 20px;
+        }
+      }
+    }
+  }
 `;
