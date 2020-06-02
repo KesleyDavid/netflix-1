37d9046bb4095bb876bd9ee5c60f3bd389446c41
Willian Tavares <willianliketavares@gmail.com>
2020-06-02 18:25:35 -0300

    improve code

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import Register from './pages/Register';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 
 const App: React.FC = () => (
   <>
     {/* <Home /> */}
 
-    <Register />
+    <Home />
     <GlobalStyle />
   </>
 );
